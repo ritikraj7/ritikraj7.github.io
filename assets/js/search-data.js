bd1ce95464d646be200cb92f8c4b35014b09f39f
ritikraj7 <54772312+ritikraj7@users.noreply.github.com>
2025-10-19 07:59:30 +0000

    Deploying to gh-pages from @ ritikraj7/ritikraj7.github.io@100a3cc6d1fd9037c553963ba971dd6469ad9635 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,18 +440,12 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-announcement-1",
+          title: 'Announcement_1',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+              window.location.href = "/news/announcement_1/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
